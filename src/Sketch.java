3c69aa0a8aa9a5b8c16a652fa3cd5f73fa639c1d
CollinJin09 <yangzhe.weng27@ycdsbk12.ca>
2026-06-01 13:47:55 -0400

    Refactored Show Text Functions

--- src/Sketch.java
+++ src/Sketch.java
@@ -12,11 +12,13 @@ public class Sketch extends PApplet {
     int randomIndex;
     boolean playerTurn = true; 
     boolean wouldYouLikePlayAgain = false;
+    String gameMessage = "Would you like to hit or stay? (h/s)";
+    int playerSum;
+    int dealerSum;
 
     int [] deck = {11,2,3,4,5,6,7,8,9,10,10,10,10};
     String [] cardName = {"Ace", "2","3", "4","5","6","7","8","9","10", "Jack", "Queen", "King"};
     int card; 
-    int aceCount = 0;
 
 
     public static void main(String[] args) {
@@ -34,46 +36,65 @@ public class Sketch extends PApplet {
 
     @Override
     public void setup() {
+        background(0);  //Black 
         gameStart();
     }
 
     @Override
     public void draw() {
-        playerAndDealerTurn();
+        showHands();
     }
 
     private void gameStart(){
-        background(0);  //Black 
-        fill(255);
+        fill (255);
         textSize(20);
-        println();
-        println("=========Black Jack=========");
-        println();
-        for (int i = 0; i < 2; i++){
-            playerHand.add(deck[randomDeckIndex()]); // Adds random card
+        text(gameMessage, 20, 90);
+        playerTurn = true;
+
+        for (int i = 0; i < 2; i++){                    // Draws 2 random cards for dealer and player
+            playerHand.add(deck[randomDeckIndex()]); 
             dealerHand.add(deck[randomDeckIndex()]);
+        }
+
+        if (playerSum >= 21 || dealerSum >= 21){        // Checks for immediate winner
+            playerTurn = false;
+            determineWinner();
+            
         }
     }
 
-    private void playerAndDealerTurn(){
+    private void showHands(){
+        background(0);
+        playerSum = getSum(playerHand);
+        dealerSum = getSum(dealerHand);
+
+        fill(255);
+        textSize(20);
+        text("You have: " + playerHand + ", sum: " + playerSum, 20, 30);
+
         if (playerTurn){
-            playerTurn();
+            text("The dealer reveals " + dealerHand.get(0) + " and [?] ", 20, 60);
         }
         else {
-            while (getSum(dealerHand)<17){
-               dealerTurn(); 
-            }
-            determineWinner();
+            text("The dealer reveals " + dealerHand + ", sum: " + dealerSum, 20, 60);
         }
+
+        text(gameMessage, 20, 110);
     }
 
     public void keyPressed() {
-        // Note: It's safer to use the logical AND (&&) instead of bitwise AND (&)
         if (key == 'h' && playerTurn) {
             playerHand.add(deck[randomDeckIndex()]); 
+            playerSum = getSum(playerHand);
+
+            if (playerSum >= 21){
+                dealerTurn();
+            }
         }
         if (key == 's' && playerTurn){
+            background(0);
             playerTurn = false;
+            dealerTurn();
         }
         if (key == 'y' && wouldYouLikePlayAgain){
             wouldYouLikePlayAgain();
@@ -85,47 +106,47 @@ public class Sketch extends PApplet {
         }
     }
 
-    private void playerTurn(){
-        showHands();
-        text("Would you like to hit or stay? (h/s)", 20, 90);
-        playerTurn = true;
-        if (getSum(playerHand) >= 21){
-            determineWinner();
-        }
-    }
-
     private void dealerTurn(){
-        showHands();
-        dealerHand.add(deck[randomDeckIndex()]);
+        while (dealerSum < 17){
+            dealerHand.add(deck[randomDeckIndex()]);
+            dealerSum = getSum(dealerHand);
+        } 
+        determineWinner();
     }
 
     private void determineWinner(){
-        showHands();
         boolean playerWon;
+        playerSum = getSum(playerHand);
+        dealerSum = getSum(dealerHand);
         
-        if (getSum(playerHand)==21||getSum(dealerHand)>21){
+        if (playerSum == 21||dealerSum > 21){
             playerWon = true;
-            text("WINNER WINNER WINNER", 20, 90);
+            gameMessage = "WINNER WINNER WINNER";
         } 
-        else if(getSum(playerHand)>21||getSum(dealerHand)==21){
+        else if(playerSum > 21|| dealerSum == 21){
             playerWon = false;
-            text("HOUSE WINS " , 20, 90);
+            gameMessage = "HOUSE WINS ";
         } 
-        else if (getSum(playerHand)>getSum(dealerHand)){
+        else if(playerSum == dealerSum){
+            playerWon = false;
+            gameMessage = "TIE, Bets have been returned ";
+        } 
+        else if (playerSum > dealerSum){
             playerWon = true;
-            text("WINNER WINNER WINNER", 20, 90);
+            gameMessage = "WINNER WINNER WINNER";
         }
         else{
             playerWon = false;
-            text("HOUSE WINS " , 20, 90);
+            gameMessage = "HOUSE WINS ";
         }
-
-        wouldYouLikePlayAgain = true; 
-        text("Would You like to Play Again? (y/n)" , 20, 120);
+        
+        gameMessage += ". Would you like to play again? (y/n)"; 
+        wouldYouLikePlayAgain = true;
     }
 
     private int getSum(ArrayList <Integer> hand){
         int sum = 0; 
+        int aceCount = 0;
         for (int card : hand){
             sum += card;
             if (card == 11){
@@ -139,20 +160,10 @@ public class Sketch extends PApplet {
         return sum;
     }
 
-    private void showHands(){
-        background(0); 
-        fill(255);
-        textSize(20);
-        text("You have: " + playerHand + ", sum: " + getSum(playerHand), 20, 30);
-        text("The dealer reveals " + dealerHand + getSum(dealerHand), 20, 60);
-    }
-
-    private void wouldYouLikePlayAgain(){       
+    private void wouldYouLikePlayAgain(){
         playerHand.clear();
         dealerHand.clear();
         gameStart();
-        playerAndDealerTurn();
-        playerTurn = true;
     }
 
 }
